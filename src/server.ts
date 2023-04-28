ea022ee4b16d690d2f89182d66f7ce50de094a9f
pedro junior <pedro.junior@infarma.com.br>
2023-04-28 13:52:52 -0300

    ajustes na rota de getHabitsDay

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import Fastify from "fastify";
+import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { appRoutes } from "./routes";
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-app.register(cors)
 
+app.register(cors)
 app.register(appRoutes)
 
 app.listen({
-  port: 3333
+  port: 3333,
+  host: '0.0.0.0'
 }).then(() => {
-  console.log('HTTP server is running')
+  console.log('HTTP Server running!')
 })
